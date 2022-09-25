7d2a91494af5362acdb771c6e4b2225565c9f2b1
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-09-25 09:49:40 +0700

    Đổi query

--- Trunk/Sever/app/src/main/java/com/dtshop/app/Repository/RoleUserRepository.java
+++ Trunk/Sever/app/src/main/java/com/dtshop/app/Repository/RoleUserRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoleUserRepository extends JpaRepository<RoleUser, Long> {
-    @Query("Select entity From RoleUser entity Where entity.user.id = ?1")
+    @Query("Select entity.user From RoleUser entity Where entity.user = ?1")
     RoleUser getUserByUsername(String username);
+
+    @Query("Select entity.role From RoleUser entity Where entity.user = ?1")
+    List<RoleUser> getRoleByUserName(String username);
 }
